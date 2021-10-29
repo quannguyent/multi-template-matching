3ce3080ab630d95794dc05fc7aa661854023b400
quannguyent <49954085+quannguyent@users.noreply.github.com>
2021-10-29 14:04:38 +0700

    Add files via upload

---
+++ multi_template_matching.py
@@ -0,0 +1,72 @@
+from nms import non_max_suppression_fast
+import argparse
+import cv2
+import numpy as np
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, required=True,
+	help="path to input image where we'll apply template matching")
+ap.add_argument("-t", "--template", type=str, required=True,
+	help="path to template image")
+ap.add_argument("-b", "--threshold", type=float, default=0.8,
+	help="threshold for multi-template matching")
+args = vars(ap.parse_args())
+
+# load the input image and template image from disk, then grab the
+# template image spatial dimensions
+print("[INFO] loading images...")
+image = cv2.imread(args["image"])
+template = cv2.imread(args["template"])
+(tH, tW) = template.shape[:2]
+# display the  image and template to our screen
+# cv2.imshow("Image", image)
+cv2.imshow("Template", template)
+
+# convert both the image and template to grayscale
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+# perform template matching
+print("[INFO] performing template matching...")
+result = cv2.matchTemplate(imageGray, templateGray,
+	cv2.TM_CCOEFF_NORMED)
+
+# find all locations in the result map where the matched value is
+# greater than the threshold, then clone our original image so we
+# can draw on it
+(yCoords, xCoords) = np.where(result >= args["threshold"])
+clone = image.copy()
+print("[INFO] {} matched locations *before* NMS".format(len(yCoords)))
+# loop over our starting (x, y)-coordinates
+for (x, y) in zip(xCoords, yCoords):
+	# draw the bounding box on the image
+	cv2.rectangle(clone, (x, y), (x + tW, y + tH),
+		(255, 0, 0), 3)
+# show our output image *before* applying non-maxima suppression
+cv2.imshow("Before NMS", clone)
+cv2.waitKey(0)
+
+# initialize our list of rectangles
+rects = []
+# loop over the starting (x, y)-coordinates again
+for (x, y) in zip(xCoords, yCoords):
+	# update our list of rectangles
+	rects.append((x, y, x + tW, y + tH))
+# apply non-maxima suppression to the rectangles
+pick = non_max_suppression_fast(np.array(rects))
+print("[INFO] {} matched locations *after* NMS".format(len(pick)))
+# loop over the final bounding boxes
+count = 0
+for (startX, startY, endX, endY) in pick:
+	# draw the bounding box on the image
+	count += 1
+	cv2.rectangle(image, (startX, startY), (endX, endY),(0,0,0), 1)
+	cv2.putText(image, str(count), (startX, startY-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+	
+# show the output image
+cv2.imshow("After NMS", image)
+# cv2.imwrite('C:/Users/Quan/Desktop/count_template.jpg', template)
+# cv2.imwrite('C:/Users/Quan/Desktop/count.jpg', image)
+# cv2.imwrite('C:/Users/Quan/Desktop/find_template.jpg', template)
+# cv2.imwrite('C:/Users/Quan/Desktop/find.jpg', image)
+cv2.waitKey(0)
